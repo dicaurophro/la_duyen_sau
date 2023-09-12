f9483ff41c83acf000f51c4cf37bf3ed6371d59f
dicaurophro <144820716+dicaurophro@users.noreply.github.com>
2023-09-12 15:02:22 +0000

    cn

---
+++ p-i-x-i-v.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         p-i-x-i-v
+// @namespace    https://github.com/dicaurophro/la_duyen_sau/raw/main/p-i-x-i-v.user.js
+// @version      2023.09.12
+// @description  down p-i-x-i-v
+// @author       You
+// @match        https://comic.pixiv.net/viewer/stories/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=comic.pixiv.net
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/dicaurophro/la_duyen_sau/raw/main/p-i-x-i-v.user.js
+// @updateURL    https://github.com/dicaurophro/la_duyen_sau/raw/main/p-i-x-i-v.meta.js
+// ==/UserScript==
