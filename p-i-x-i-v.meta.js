5c4eb2c484fd779164a3034bde93388ac47fd821
dicaurophro <144820716+dicaurophro@users.noreply.github.com>
2023-09-13 14:04:59 +0700

    cn

--- p-i-x-i-v.meta.js
+++ p-i-x-i-v.meta.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         p-i-x-i-v
 // @namespace    https://github.com/dicaurophro/la_duyen_sau/raw/main/p-i-x-i-v.user.js
-// @version      2023.09.13
+// @version      2023.09.14
 // @description  down p-i-x-i-v
 // @author       You
 // @match        https://comic.pixiv.net/viewer/stories/*
+// @match        https://api.distribution.mediadotech.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=comic.pixiv.net
 // @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/fast-xml-parser/4.2.7/fxparser.min.js
 // @grant        none
 // @downloadURL  https://github.com/dicaurophro/la_duyen_sau/raw/main/p-i-x-i-v.user.js
 // @updateURL    https://github.com/dicaurophro/la_duyen_sau/raw/main/p-i-x-i-v.meta.js
